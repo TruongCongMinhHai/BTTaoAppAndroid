cfc9c334ddea54eaa2d370523501066f32cc996a
Trương Công Minh Hải <conconghai@gmail.com>
2026-09-17 21:53:35 +0700

    Step 4: Lap trinh MainActivity.kt

--- StudentProfileCard/app/src/main/java/com/ute/studentprofile/MainActivity.kt
+++ StudentProfileCard/app/src/main/java/com/ute/studentprofile/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.ute.studentprofile
-
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
+import com.ute.studentprofile.databinding.ActivityMainBinding
+import com.ute.studentprofile.model.Student
+import com.ute.studentprofile.utils.toAcademicRanking
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var currentStudent = Student(
+        id = "2415053122315",
+        name = "Truong Cong Minh Hai",
+        className = "126LTTD01",
+        email = "2415053122315@sv.ute.udn.vn",
+        gpa = 3.8
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        bindStudentData(currentStudent)
+    }
+    private fun bindStudentData(student: Student) {
+        with(binding) {
+            tvName.text = student.name
+            tvStudentId.text = "MSSV: ${student.id} • Lớp: ${student.className}"
+            tvGpaBadge.text = "GPA ${student.gpa} (${student.gpa.toAcademicRanking()})"
+            edtNewGpa.setText(student.gpa.toString())
         }
     }
 }
